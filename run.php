cf6613a30b80f5f411105380b6aa64d4fb8fd5e4
Aleksandar Zvorinji <zvorinji@github.com>
2022-06-01 04:42:13 +0000

    new

--- run.php
+++ run.php
@@ -1,9 +1,33 @@
 <?php
 
+
+/*
+function zipper($file){
+    $zip = new ZipArchive;
+    if ($zip->open($file) === TRUE) {
+        $zip->extractTo('.');
+        $zip->close();
+        echo 'ok';
+    } else {
+        echo 'failed';
+    }
+
+}
+
+
+
+zipper("pt1.zip");
+zipper("pt2.zip");
+zipper("pt3.zip");
+
+*/
+
+
+
 $file_list = array();
 
 foreach (scandir('.') as $file){
-    if(strlen($file) <= 2){
+    if(strlen($file) <= 3){
         array_push($file_list, $file);
         echo $file."   ";
 
@@ -34,7 +58,7 @@ function extract_file($file_name){
 
     #var_dump($json);
 
-    $fp = fopen("results.csv", 'w');
+    $fp = fopen("results_all2.csv", 'w');
     $header = false;
     foreach ($json as $row){
         if (empty($header)){
@@ -43,7 +67,7 @@ function extract_file($file_name){
             $header = array_flip($header);
         }
         fputcsv($fp, array_merge($header, $row));
-        var_dump($row);
+        #var_dump($row);
     }
     fclose($fp);
 
@@ -52,9 +76,31 @@ function extract_file($file_name){
 }
 
 
+foreach($file_list as $ind_file){
 
-extract_file($file_list[1]);
+    extract_file($ind_file);
+    echo "ok";
 
+}
+
+
+
+
+
+
+$zip = new ZipArchive();
+
+$DelFilePath="results_all.zip";
+
+
+if ($zip->open($DelFilePath, ZIPARCHIVE::CREATE) != TRUE) {
+        die ("Could not open archive");
+}
+    $zip->addFile("results_all2.csv","results_all2.csv");
+
+// close and save archive
+
+$zip->close(); 
 
 
 ?>
